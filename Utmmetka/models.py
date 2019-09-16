67421cc36734087820c43ecdf8d34adc64d4a790
razumovr <47295140+razumovr@users.noreply.github.com>
2019-09-16 18:17:11 +0300

    Update models.py

--- Utmmetka/models.py
+++ Utmmetka/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.TextField()
 
     def __str__(self):
         return self.name
@@ -10,7 +10,7 @@ class Country(models.Model):
 
 class City(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    name = models.CharField(max_length=60)
+    name = models.TextField()
 
     def __str__(self):
         return self.name
