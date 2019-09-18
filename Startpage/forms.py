08dd9f53e3932e895eab77006a98e0e89a5b783e
bortmehannik <paketonos2@gmail.com>
2019-09-18 14:54:34 +0300

    Clear HTML

--- Startpage/forms.py
+++ Startpage/forms.py
@@ -5,33 +5,25 @@ class LangingForm(ModelForm):
     class Meta:
         model=Langing
         fields=['land','success','start','end','complete','heshteg',]
-
-
         widgets={'land':TextInput(attrs={'class': '',
-                                         'name':'компания',
-                                         'placeholder':"Введите лендинг"
+                                         'name':'компания'
                                          }
                                   ),
                  'success': TextInput(attrs={'class': '',
-                                          'name': 'компания',
-                                          'placeholder': "Введите success"
+                                          'name': 'компания'
                                           }),
                  'start': TextInput(attrs={'class': '',
-                                          'name': 'компания',
-                                          'placeholder': "Введите начальную дату"
+                                          'name': 'компания'
                                           }
                                    ),
                  'end': TextInput(attrs={'class': '',
-                                             'name': 'компания',
-                                             'placeholder': "Введите конечную дату"
+                                             'name': 'компания'
                                              }),
                  'complete': TextInput(attrs={'class': '',
-                                          'name': 'компания',
-                                          'placeholder': "Введите completesheet"
+                                          'name': 'компания'
                                           }
                                    ),
                  'heshteg': TextInput(attrs={'class': '',
-                                             'name': 'компания',
-                                             'placeholder': "Введите хештег"
+                                             'name': 'компания'
                                              })
                  }
