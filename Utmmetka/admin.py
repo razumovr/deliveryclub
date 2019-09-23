e1ebbe8d29bf823c80e129cfbdc067c1d400b459
razumovr <47295140+razumovr@users.noreply.github.com>
2019-09-23 13:16:56 +0300

    Update admin.py

--- Utmmetka/admin.py
+++ Utmmetka/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Country,City,Person,Firstvar
+from .models import Country,City,Person,Firstvar,Urlname
 
 admin.site.register(Country)
 admin.site.register(City)
 admin.site.register(Person)
 admin.site.register(Firstvar)
+admin.site.register(Urlname)
+
