8b1eadfe0126d0c80e85a36a3e8b57f7e9528310
razumovr <47295140+razumovr@users.noreply.github.com>
2019-09-23 13:18:10 +0300

    Update urls.py

--- Utmmetka/urls.py
+++ Utmmetka/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.PersonListView.as_view(), name='person_changelist'),
+    path('pageurl/', views.inserturl,name='url_add'),
+    path('pageurl/update', views.inserturl2),
     path('add/pagenext', views.index2),
     path('add/', views.PersonCreateView.as_view(), name='person_add'),
     path('ajax/load-cities/', views.load_cities, name='ajax_load_cities'),
