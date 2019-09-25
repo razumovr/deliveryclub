bf4642598185627dec84e2f93d68150c2c485300
razumovr <47295140+razumovr@users.noreply.github.com>
2019-09-25 13:46:01 +0300

    Update urls.py

--- Utmmetka/urls.py
+++ Utmmetka/urls.py
@@ -1,13 +1,16 @@
 from django.urls import include, path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
-    path('', views.PersonListView.as_view(), name='person_changelist'),
-    path('pageurl/', views.inserturl,name='url_add'),
-    path('pageurl/update', views.inserturl2),
-    path('add/pagenext', views.index2),
-    path('add/', views.PersonCreateView.as_view(), name='person_add'),
-    path('ajax/load-cities/', views.load_cities, name='ajax_load_cities'),
-    path('add/utmupdate',views.index3)
+    path('', login_required(views.PersonListView.as_view()), name='person_changelist'),
+    path('pageurl/', login_required(views.inserturl),name='url_add'),
+    path('pageurl/update', login_required(views.inserturl2)),
+    path('add/pagenext', login_required(views.index2)),
+    path('add/', login_required(views.PersonCreateView.as_view()), name='person_add'),
+    path('ajax/load-cities/', login_required(views.load_cities), name='ajax_load_cities'),
+    path('add/utmupdate',login_required(views.index3))
 ]
+
+
