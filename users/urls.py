b06356868816f04d16bc38e1bc27d801487345fb
razumovr <47295140+razumovr@users.noreply.github.com>
2019-09-25 22:03:20 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "users"
 urlpatterns = [
   path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
+  path('logout/', views.logout_view, name="logout"),
 ]
