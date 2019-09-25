01efb2ea36daa9aacda5478f2e54d69c69b6e35a
razumovr <47295140+razumovr@users.noreply.github.com>
2019-09-25 13:44:47 +0300

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.views import LoginView
+
+from django.urls import path
+from . import views
+app_name = "users"
+urlpatterns = [
+  path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
+]
