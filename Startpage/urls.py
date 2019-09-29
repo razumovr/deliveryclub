d7dddb94b81767eb532b7653e3fd79380a3b350c
razumovr <47295140+razumovr@users.noreply.github.com>
2019-09-30 00:13:51 +0300

    Create urls.py

---
+++ Startpage/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.decorators import login_required
+
+
+urlpatterns = [
+     path('', login_required(views.indexstartpage),name='start'),
+     path('funnel', login_required(views.index)),
+     path('page2', login_required(views.index2)),
+     ]
