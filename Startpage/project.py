cd949ab680f326fe63dac70e125ab66ae6a9d908
razumovr <47295140+razumovr@users.noreply.github.com>
2019-10-01 01:11:47 +0300

    Update project.py

--- Startpage/project.py
+++ Startpage/project.py
@@ -19,20 +19,7 @@ from conecttosheets import connect,connectsheet,connectIP
 
 
 
-
-
-
 def infopartnerip(urlland):
-    '''scope = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.com/feeds']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        '/Users/ruslan/Downloads/My Project-fe4805e9d102.json', scope)
-    gc = gspread.authorize(credentials)
-    ippartner = gc.open_by_url(
-        'https://docs.google.com/spreadsheets/d/1WmDnz3794uoU_h7ho_hOPMISUA2CdXt_UA_8L9pcJ-s/edit?pli=1#gid=0').sheet1
-    dfip = get_as_dataframe(ippartner, header=0)'''
-    #Sortirovat po datam dock Kris
-    #start2=datetime.date(int(start[:4]), int(start[5:7]), int(start[8:10]))
-    #stop2=datetime.date(int(stop[:4]), int(stop[5:7]), int(stop[8:10]))
     dfip=connectIP('https://docs.google.com/spreadsheets/d/1WmDnz3794uoU_h7ho_hOPMISUA2CdXt_UA_8L9pcJ-s/edit?pli=1#gid=0')
     costdf = dfip[dfip.values == urlland].loc[:, 'Стоимость ']
     ipKOLICHESTVO = len(costdf)
@@ -41,93 +28,30 @@ def infopartnerip(urlland):
         sumcostdf += int(costdf[i])
     return [ipKOLICHESTVO,sumcostdf]
 
-
-
-'''def kolvopicsfunct(k):
-    unikalkarry = []
-    digestarray = []
-    telegaarray = []
-    # lif 'digest' in i or 'Digest' in i:
-    #  elif 'tg /' in i or 'Tg /' in i:
-    for i in range(len(k)):
-        for j in range(len(k[i])):
-            if 'generalbase' in str(k[i][j]) or 'mailchimp' in str(k[i][j]):
-                unikalkarry.append(k[i][j + 1])
-            elif 'digest' in str(k[i][j]) or 'Digest' in str(k[i][j]):
-                digestarray.append(k[i][j + 1])
-            elif 'tg /' in str(k[i][j]) or 'Tg /' in str(k[i][j]):
-                telegaarray.append(k[i][j + 1])
-            else:
-                pass
-    #  if 'generalbase' in ii or 'mailchimp' in ii:
-
-    max_value = 0
-    for n in unikalkarry:
-        if n > max_value:
-            max_value = n
-    if max_value == 1:
-        lenUNIKALKA = sg.find_peaks_cwt(unikalkarry, np.arange(1, int(max_value + 1)),
-                                        max_distances=np.arange(1, int(max_value + 1)))
-    elif max_value == 0:
-        lenUNIKALKA = []
-    else:
-        lenUNIKALKA = sg.find_peaks_cwt(unikalkarry, np.arange(1, int(max_value)),
-                                        max_distances=np.arange(1, int(max_value)))
-
-    max_value = 0
-    for n in digestarray:
-        if n > max_value:
-            max_value = n
-    if max_value == 1:
-        lenDIGEST = sg.find_peaks_cwt(digestarray, np.arange(1, int(max_value + 1)),
-                                      max_distances=np.arange(1, int(max_value + 1)))
-    elif max_value == 0:
-        lenDIGEST = []
-    else:
-        lenDIGEST = sg.find_peaks_cwt(digestarray, np.arange(1, int(max_value)),
-                                      max_distances=np.arange(1, int(max_value)))
-
-    max_value = 0
-    for n in telegaarray:
-        if n > max_value:
-            max_value = n
-    if max_value == 1:
-        lenTELEGA = sg.find_peaks_cwt(telegaarray, np.arange(1, int(max_value + 1)),
-                                      max_distances=np.arange(1, int(max_value + 1)))
-    elif max_value == 0:
-        lenTELEGA = []
-    else:
-        lenTELEGA = sg.find_peaks_cwt(telegaarray, np.arange(1, int(max_value)),
-                                      max_distances=np.arange(1, int(max_value)))
-
-
-
-    return [len(lenUNIKALKA),len(lenDIGEST),len(lenTELEGA)]'''
-
-
-
-
 def main():
     landing = Langing.objects.all()
-    #Langing.objects.all()
-    #from .models import Langing
 
-    #d=analitica(landing)
 
     q = Queue(connection=conn)
     q1 = Queue(connection=conn)
-    q2targeting = Queue(connection=conn)
+    #q2targeting = Queue(connection=conn)
     result = q.enqueue(analitica,str(landing[0].land),str(landing[0].success),str(landing[0].start),str(landing[0].end),str(landing[0].complete))
     result1 = q1.enqueue(colvodneyforday,str(landing[0].start),str(landing[0].end),str(landing[0].land))
-    result2 = q2targeting.enqueue(connectsheet,'https://docs.google.com/spreadsheets/d/1lcHMPIw1AtzKx3DoFAVp_JDi2Cb_-DbP9krjtD7c69Q/edit#gid=237212384',str(landing[0].start),str(landing[0].land))
-    
-    #df=connectsheet('https://docs.google.com/spreadsheets/d/1lcHMPIw1AtzKx3DoFAVp_JDi2Cb_-DbP9krjtD7c69Q/edit#gid=237212384',datastart)
+    #result2 = q2targeting.enqueue(connectsheet,'https://docs.google.com/spreadsheets/d/1lcHMPIw1AtzKx3DoFAVp_JDi2Cb_-DbP9krjtD7c69Q/edit#gid=237212384',str(landing[0].start),str(landing[0].land))
+    result2 = connectsheet('https://docs.google.com/spreadsheets/d/1lcHMPIw1AtzKx3DoFAVp_JDi2Cb_-DbP9krjtD7c69Q/edit#gid=237212384',str(landing[0].start),str(landing[0].land))
 
+    try:
+        infopartenrilist=infopartnerip('https://1.changellenge.com/supply-chain'))
+        '''d['Количество'][3] = infopartenrilist[0]
+        d['Бюджетфакт'][3] = infopartenrilist[1]'''
+    except:
+        pass
     time.sleep(20)
     print("REZULT"*100)
     print(result.result)
     print(result1.result)
-    print(result2.result)
+    print(result2)
+    print(infopartenrilist)
                                  
                          
     
