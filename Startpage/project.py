1f78151f7eabe563073d3533b96c0ce4aea05eb9
razumovr <47295140+razumovr@users.noreply.github.com>
2019-10-03 17:43:58 +0300

    Update project.py

--- Startpage/project.py
+++ Startpage/project.py
@@ -94,7 +94,7 @@ def main():
     q2 = Queue(connection=conn)
     
     #q2targeting = Queue(connection=conn)
-    result = q.enqueue(analitica,str(landing[0].land),str(landing[0].success),str(landing[0].start),str(landing[0].end),str(landing[0].complete))
+    result = q.enqueue(analitica,str(landing[0].land),str(landing[0].success),str(landing[0].start),str(landing[0].end))
     result1 = q1.enqueue(colvodneyforday,str(landing[0].start),str(landing[0].end),str(landing[0].land))
     try:
         connecttocomplete = googlesheets(str(landing[0].complete))
